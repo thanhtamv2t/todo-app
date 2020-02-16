66817eb635c350c0e61dece8062d038b4b09f9ca
Thanh Tam <thanhtam.v2t@gmail.com>
2020-02-16 09:01:08 +0700

    Todo-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import uuid from "uuid";
+// import "./App.scss";
+import styles from "./App.module.scss";
 
-function App() {
+const App = () => {
+  const fromLocal = JSON.parse(localStorage.getItem("todos"));
+  const [state, setState] = useState({
+    todos: fromLocal || [],
+    todo: ""
+  });
+
+  const saveLocal = todos => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const handleInputChange = e => {
+    const { value } = e.target;
+    return setState({ ...state, todo: value });
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    const { todo, todos } = state;
+    if (!todo) return;
+    const newTodo = {
+      id: uuid(),
+      todo,
+      completed: false
+    };
+    saveLocal([...todos, newTodo]);
+    return setState({ ...state, todo: "", todos: [...todos, newTodo] });
+  };
+
+  const handleTodoClick = todo => {
+    const { todos } = state;
+    const index = todos.findIndex(item => item.id === todo.id);
+    todos[index].completed = !todos[index].completed;
+    saveLocal(todos);
+    return setState({ ...state, todos });
+  };
+
+  const handleDeleteTodo = todo => {
+    const todos = state.todos.filter(item => item.id !== todo.id);
+    saveLocal(todos);
+    return setState({ ...state, todos });
+  };
+
+  const handleCleanCompleted = () => {
+    const todos = state.todos.filter(item => item.completed === false);
+    saveLocal(todos);
+    return setState({ ...state, todos });
+  };
+
+  const renderTodosList = status => {
+    const { todos } = state;
+    return (
+      todos.length > 0 &&
+      todos
+        .filter(item => item.completed === status)
+        .map(item => (
+          <li key={item.id}>
+            <span onClick={() => handleTodoClick(item)}>{item.todo}</span>
+            <button
+              className={styles.closeButton}
+              onClick={() => handleDeleteTodo(item)}
+            >
+              <i className="lni-trash"></i>
+            </button>
+          </li>
+        ))
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.app}>
+      <form className={styles.form} onSubmit={e => handleFormSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Enter things to do"
+          value={state.todo}
+          onChange={e => handleInputChange(e)}
+        />
+        <button
+          type="button"
+          className={`
+            ${styles.clearButton} ${
+            state.todo.length > 0 ? null : styles.hidden
+          }
+          `}
+          onClick={() => setState({ ...state, todo: "" })}
         >
-          Learn React
-        </a>
-      </header>
+          <i className="lni-close"></i>
+        </button>
+      </form>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <h3>Todo</h3>
+          <ul className={styles.todoList}>{renderTodosList(false)}</ul>
+        </div>
+        <div className={styles.col}>
+          <h3>Completed</h3>
+          <ul className={styles.completedList}>{renderTodosList(true)}</ul>
+          <button
+            className={`${styles.cleanButton} ${
+              state.todos.filter(item => item.completed === true).length > 0
+                ? ""
+                : styles.hidden
+            }`}
+            onClick={() => handleCleanCompleted()}
+          >
+            Clean Completed
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
